77b0865c95cf6a0ea764f00e94112103a319f8b9
andreabican <andreeabican@yahoo.com>
2016-08-10 16:54:55 +0300

    Renamed properties

--- OAuthClient/src/main/java/Andreea/Bican/Commands/LoginGoogleEndpoint.java
+++ OAuthClient/src/main/java/Andreea/Bican/Commands/LoginGoogleEndpoint.java
@@ -3,20 +3,25 @@ package Andreea.Bican.Commands;
 import Andreea.Bican.ICommand;
 
 import java.awt.*;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by andre on 10.08.2016.
  */
 public class LoginGoogleEndpoint implements ICommand{
 
-    private final String uriString = "http://localhost:8181/login/google";
+    private final String targetURL = "http://localhost:8181/login/google";
 
     @Override
     public void execute() throws URISyntaxException {
+
         Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
-        URI uri = new URI(uriString);
+        URI uri = new URI(targetURL);
         if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
             try {
                 desktop.browse(uri);

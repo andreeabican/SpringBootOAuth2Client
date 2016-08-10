ae59daf0b1c553c9d68e4b64c22b534b9f566b59
andreabican <andreeabican@yahoo.com>
2016-08-10 15:28:40 +0300

    Added method for login/Facebook and login/Google endpoints

--- OAuthClient/src/main/java/Andreea/Bican/CommandParser.java
+++ OAuthClient/src/main/java/Andreea/Bican/CommandParser.java
@@ -1,5 +1,7 @@
 package Andreea.Bican;
 
+import Andreea.Bican.Commands.AutoLoginEndpoint;
+import Andreea.Bican.Commands.LoginFacebookEndpoint;
 import Andreea.Bican.Commands.LoginGoogleEndpoint;
 
 /**
@@ -8,8 +10,12 @@ import Andreea.Bican.Commands.LoginGoogleEndpoint;
 public class CommandParser {
 
     public ICommand getCommand(String input){
-        if(input.startsWith("/loginGoogle")){
+        if(input.toLowerCase().startsWith("/logingoogle")){
+            return new AutoLoginEndpoint();
+        }else if(input.toLowerCase().startsWith("/login/google")){
             return new LoginGoogleEndpoint();
+        }else if(input.toLowerCase().startsWith("/login/facebook")) {
+            return new LoginFacebookEndpoint();
         }else{
             return null;
         }
